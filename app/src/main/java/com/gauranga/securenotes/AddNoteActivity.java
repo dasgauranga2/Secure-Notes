ccebc0f703990696f354071c51fd84947a5f99eb
Gauranga Das <dasgauranga2@gmail.com>
2021-03-01 11:38:14 +0530

    Add notes to the SQLite database

--- app/src/main/java/com/gauranga/securenotes/AddNoteActivity.java
+++ app/src/main/java/com/gauranga/securenotes/AddNoteActivity.java
@@ -2,13 +2,55 @@ package com.gauranga.securenotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddNoteActivity extends AppCompatActivity {
+
+    EditText title,content;
+
+    public void save_note(View view) {
+        // get the title and content entered by the user
+        String title_text = title.getText().toString();
+        String content_text = content.getText().toString();
+
+        // check if the title/content is empty
+        if (title_text.length()==0) {
+            Toast.makeText(this, "TITLE EMPTY", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (content_text.length()==0) {
+            Toast.makeText(this, "CONTENT EMPTY", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        try {
+            // creates a new database or open an existing database
+            SQLiteDatabase database = this.openOrCreateDatabase("NOTES",MODE_PRIVATE,null);
+            // create a table if one already does not exists
+            // and specify the fields of the table and their types
+            database.execSQL("CREATE TABLE IF NOT EXISTS test (title VARCHAR, content VARCHAR)");
+            // insert data into the table
+            database.execSQL("INSERT INTO test (title,content) VALUES ('"+title_text+"','"+content_text+"')");
+            // go back to Main Activity
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_note);
+
+        title = findViewById(R.id.titleInputText);
+        content = findViewById(R.id.contentInputText);
     }
 }
--- app/src/main/java/com/gauranga/securenotes/MainActivity.java
+++ app/src/main/java/com/gauranga/securenotes/MainActivity.java
@@ -3,7 +3,10 @@ package com.gauranga.securenotes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,5 +21,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            // creates a new database or open an existing database
+            SQLiteDatabase database = this.openOrCreateDatabase("NOTES",MODE_PRIVATE,null);
+
+            // cursor is used to retrieve data from the table
+            // below SQL query selects all the rows from the table
+            Cursor c = database.rawQuery("SELECT * FROM test", null);
+            // 'title' column index
+            int title_index = c.getColumnIndex("title");
+            // 'content' column index
+            int content_index = c.getColumnIndex("content");
+
+            // move the cursor to the beginning
+            c.moveToFirst();
+            while (c != null) {
+                // retrieve data from the table using the cursor and column index
+                Log.i("NOTES_DATA", c.getString(title_index) + "--" + c.getString(content_index));
+                c.moveToNext();
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
