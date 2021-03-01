ac7f061c0177c6e30aec65441cf24b1277500150
Gauranga Das <dasgauranga2@gmail.com>
2021-03-01 09:26:16 +0530

    AddNote Activity for adding notes

--- app/src/main/java/com/gauranga/securenotes/MainActivity.java
+++ app/src/main/java/com/gauranga/securenotes/MainActivity.java
@@ -2,9 +2,17 @@ package com.gauranga.securenotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    // add a new note
+    public void add_note(View view) {
+        Intent intent = new Intent(getApplicationContext(),AddNoteActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
