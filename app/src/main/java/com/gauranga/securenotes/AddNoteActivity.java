07cd2991e7256eeee7474a1385ca095ce0b5bbbf
Gauranga Das <dasgauranga2@gmail.com>
2021-03-02 15:13:59 +0530

    Toast messages styling

--- app/src/main/java/com/gauranga/securenotes/AddNoteActivity.java
+++ app/src/main/java/com/gauranga/securenotes/AddNoteActivity.java
@@ -4,15 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import es.dmoral.toasty.Toasty;
+
 public class AddNoteActivity extends AppCompatActivity {
 
     EditText title,content;
+    //Toast toast;
 
     public void save_note(View view) {
         // get the title and content entered by the user
@@ -21,11 +26,15 @@ public class AddNoteActivity extends AppCompatActivity {
 
         // check if the title/content is empty
         if (title_text.length()==0) {
-            Toast.makeText(this, "TITLE EMPTY", Toast.LENGTH_SHORT).show();
+            Toast toast = Toasty.custom(this, "TITLE EMPTY", R.drawable.error_icon, R.color.toast_bg_color, 500, true, true);
+            toast.setGravity(Gravity.CENTER_HORIZONTAL|Gravity.TOP, 0, (int) (content.getY()+content.getHeight())-50);
+            toast.show();
             return;
         }
         if (content_text.length()==0) {
-            Toast.makeText(this, "CONTENT EMPTY", Toast.LENGTH_SHORT).show();
+            Toast toast = Toasty.custom(this, "CONTENT EMPTY", R.drawable.error_icon, R.color.toast_bg_color, 500, true, true);
+            toast.setGravity(Gravity.CENTER_HORIZONTAL|Gravity.TOP, 0, (int) (content.getY()+content.getHeight())-50);
+            toast.show();
             return;
         }
 
@@ -44,7 +53,9 @@ public class AddNoteActivity extends AppCompatActivity {
         catch (Exception e) {
             String error_message = e.toString().toLowerCase();
             if (error_message.contains("unique") && error_message.contains("constraint")) {
-                Toast.makeText(this, "ENTER A DIFFERENT NOTE TITLE", Toast.LENGTH_SHORT).show();
+                Toast toast = Toasty.custom(this, "ENTER DIFFERENT NOTE TITLE", R.drawable.error_icon, R.color.toast_bg_color, 500, true, true);
+                toast.setGravity(Gravity.CENTER_HORIZONTAL|Gravity.TOP, 0, (int) (content.getY()+content.getHeight())-50);
+                toast.show();
             }
             Log.i("DB_ERROR", error_message);
             e.printStackTrace();
