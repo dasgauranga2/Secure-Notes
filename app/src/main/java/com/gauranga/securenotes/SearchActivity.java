f1f0c60b506f8a5de200f7317cc71ca177b13707
Gauranga Das <dasgauranga2@gmail.com>
2021-03-08 16:25:33 +0530

    Search for notes given a query entered by the user

--- app/src/main/java/com/gauranga/securenotes/SearchActivity.java
+++ app/src/main/java/com/gauranga/securenotes/SearchActivity.java
@@ -1,62 +1,74 @@
 package com.gauranga.securenotes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.content.res.ResourcesCompat;
-import androidx.fragment.app.FragmentActivity;
 import androidx.preference.PreferenceManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.Gravity;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import es.dmoral.toasty.Toasty;
 
-public class MainActivity extends AppCompatActivity {
+public class SearchActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
     List<String> titles,contents,dates;
+    EditText search;
     TextView title;
+    LinearLayout search_bg;
+    int y_offest;
 
-    // add a new note
-    public void add_note(View view) {
-        Intent intent = new Intent(getApplicationContext(),AddNoteActivity.class);
-        startActivity(intent);
-    }
-
-    // launch the settings screen
-    public void settings(View view) {
-        Intent intent = new Intent(getApplicationContext(),SettingsActivity.class);
-        startActivity(intent);
-    }
-
-    // search for notes
+    // search for all notes
+    // that contain the search query
     public void search_note(View view) {
-        Intent intent = new Intent(getApplicationContext(),SearchActivity.class);
-        startActivity(intent);
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        recyclerView = findViewById(R.id.noteListRecyclerView);
+        // initialize the lists
         titles = new LinkedList<>();
         contents =  new LinkedList<>();
         dates =  new LinkedList<>();
-        title = findViewById(R.id.titleText);
+
+        // hide the keyboard if open
+        try {
+            InputMethodManager imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        String search_query = search.getText().toString().toLowerCase();
+        // check if search query is empty
+        if (search_query.length()==0) {
+            // delay displaying the toast message
+            new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Toast toast = Toasty.custom(SearchActivity.this, "SEARCH QUERY EMPTY", R.drawable.error_icon, R.color.toast_bg_color, 500, true, true);
+                    toast.setGravity(Gravity.CENTER_HORIZONTAL|Gravity.TOP, 0, (int)recyclerView.getY()+recyclerView.getHeight()-100);
+                    toast.show();
+                }
+            }, 200);
+            return;
+        }
 
         try {
             // creates a new database or open an existing database
@@ -74,10 +86,15 @@ public class MainActivity extends AppCompatActivity {
             // move the cursor to the beginning
             c.moveToFirst();
             while (c != null) {
-                // retrieve data from the table using the cursor and column index
-                titles.add(c.getString(title_index));
-                contents.add(c.getString(content_index));
-                dates.add(c.getString(date_index));
+                // retrieve the title and content
+                String title_lower = c.getString(title_index).toLowerCase();
+                String content_lower = c.getString(content_index).toLowerCase();
+                // check if the title or the content contains the search query entered by the user
+                if (title_lower.contains(search_query) || content_lower.contains(search_query)) {
+                    titles.add(c.getString(title_index));
+                    contents.add(c.getString(content_index));
+                    dates.add(c.getString(date_index));
+                }
                 // move to the next row
                 c.moveToNext();
                 setup_recyclerview();
@@ -86,6 +103,17 @@ public class MainActivity extends AppCompatActivity {
         catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        recyclerView = findViewById(R.id.noteSearchRecyclerView);
+        search = findViewById(R.id.noteSearchText);
+        title = findViewById(R.id.searchTitleText);
+        search_bg = findViewById(R.id.searchLinearLayout);
     }
 
     @Override
@@ -102,8 +130,8 @@ public class MainActivity extends AppCompatActivity {
         Typeface tf = ResourcesCompat.getFont(this, R.font.maven);
         switch (font_family) {
             case "roboto":
-                 tf = ResourcesCompat.getFont(this, R.font.roboto_mono);
-                 break;
+                tf = ResourcesCompat.getFont(this, R.font.roboto_mono);
+                break;
             case "quicksand":
                 tf = ResourcesCompat.getFont(this, R.font.quicksand);
                 break;
@@ -125,35 +153,35 @@ public class MainActivity extends AppCompatActivity {
         String bg = sharedPreferences.getString("background","default");
         switch (bg) {
             case "blank":
-                recyclerView.setBackgroundResource(R.color.white);
+                search_bg.setBackgroundResource(R.color.white);
                 break;
             case "galaxy":
-                recyclerView.setBackgroundResource(R.drawable.galaxy_bg);
+                search_bg.setBackgroundResource(R.drawable.galaxy_bg);
                 break;
             case "night_sky":
-                recyclerView.setBackgroundResource(R.drawable.night_sky_bg);
+                search_bg.setBackgroundResource(R.drawable.night_sky_bg);
                 break;
             case "northern_lights":
-                recyclerView.setBackgroundResource(R.drawable.northern_lights_bg);
+                search_bg.setBackgroundResource(R.drawable.northern_lights_bg);
                 break;
             case "city_sunset":
-                recyclerView.setBackgroundResource(R.drawable.city_sunset_bg);
+                search_bg.setBackgroundResource(R.drawable.city_sunset_bg);
                 break;
             case "city_night":
-                recyclerView.setBackgroundResource(R.drawable.city_night_bg);
+                search_bg.setBackgroundResource(R.drawable.city_night_bg);
                 break;
             case "forest":
-                recyclerView.setBackgroundResource(R.drawable.forest_bg);
+                search_bg.setBackgroundResource(R.drawable.forest_bg);
                 break;
             case "mountain":
-                recyclerView.setBackgroundResource(R.drawable.mountain_bg);
+                search_bg.setBackgroundResource(R.drawable.mountain_bg);
                 break;
         }
     }
 
     public void setup_recyclerview() {
 
-        NoteListAdapter noteListAdapter = new NoteListAdapter(MainActivity.this, titles, contents, dates);
+        NoteListAdapter noteListAdapter = new NoteListAdapter(SearchActivity.this, titles, contents, dates);
         recyclerView.setAdapter(noteListAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
